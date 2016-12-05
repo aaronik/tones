949e9cf4d4259cd8c3f204e5123498afa8718e20
Aaron Sullivan <aasullivan1618@gmail.com>
2016-12-05 12:16:58 -0800

    More work to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,6 +10,8 @@ CHANGES=$(git log $OLD_VERSION...master --no-merges --pretty=format:"  * (%an) %
 CHANGELOG=./CHANGELOG.md
 PROG=$(basename $0)
 
+NEW_VERSION='' # will be populated later by script
+
 # for printing friendly status messages
 function report () {
   local msg="$1"
@@ -17,20 +19,25 @@ function report () {
   echo "$(tput setaf 6)$PROG: $msg$(tput sgr0)"
 }
 
+function error () {
+  local msg="$1"
+  # red this time
+  echo "$(tput setaf 1)$PROG: Fatal: $msg$(tput sgr0)"
+  exit 1
+}
+
 # ensure there are any uncommited files in the current directory
 function ensure_clean_directory () {
   report "ensuring directory is clean..."
   git diff --exit-code > /dev/null || {
-    echo 'git status must be clean before running this script, bailing like a lion.'
-    exit 1
+    error 'git working directory must be clean before running this script, bailing like a lion.'
   }
 }
 
 function ensure_on_master_branch () {
   report "ensuring we're on master branch..."
 	[[ $(git rev-parse --abbrev-ref HEAD) == 'master' ]] || {
-		echo 'must be on master branch to cut a release, bailing like a sea turtle.'
-		exit 1
+		error 'must be on master branch to cut a release, bailing like a sea turtle.'
 	}
 }
 
@@ -39,29 +46,27 @@ function ensure_on_master_branch () {
 function ensure_no_gh_pages_branch () {
   report "ensuring there isn't already a gh-pages branch..."
 	[[ $(git branch | grep gh-pages) ]] && {
-		echo 'looks like a gh-pages branch already exists, this is wrong. Bailing like a zebra.'
-		exit 1
+    error 'looks like a gh-pages branch already exists, this is wrong. Bailing like a zebra.'
 	}
 }
 
 function ensure_changes_since_last_release () {
   report "ensuring there have been changes since the last release..."
   [[ $CHANGES ]] || {
-    echo "looks like there aren't any changes since the last release, bailing like a cowardly swordfish."
-    exit 1
+    error "looks like there aren't any changes since the last release, bailing like a cowardly swordfish."
   }
 }
 
 # Prompt for new version (if not provided)
 function get_new_version () {
+  report "grabbing new version..."
+
   local proposed_version=$($SEMVER -i patch $OLD_VERSION)
 
   echo "Enter next version after $OLD_VERSION ($proposed_version):"
   read NEW_VERSION
 
   [ -z $NEW_VERSION ] && NEW_VERSION=$proposed_version
-
-  printf $NEW_VERSION
 }
 
 function print_to_changelog () {
@@ -70,26 +75,35 @@ function print_to_changelog () {
   printf "### [$new_version]\n\n" > $CHANGELOG
   echo $CHANGES > $CHANGELOG
 }
-### Main
 
-# nothing should be failing
-set -e
+# now make the build and tac it onto the release commit
+# This method keeps the master branch clean of builds.
+function amend_build () {
+  report "tacking on build to gh-pages branch..."
+  npm run build
+  git commit --amend --no-edit
+}
+
+
+### Main
 
 # technically it should already be clean and this should have no effect.
 # If it isn't, the script will bail on the next step.
 npm run clean
 
 ensure_changes_since_last_release
-ensure_clean_directory
+# ensure_clean_directory
 ensure_on_master_branch
 ensure_no_gh_pages_branch
 
+# nothing should be failing from here on out
+set -e
+
 # get the new version number
-report "grabbing new version..."
-NEW_VERSION=$(get_new_version)
+get_new_version
 
 # modify package to have new version number. Deps on .npmrc existing
-report "writing new npm version..."
+report "writing new npm version $NEW_VERSION..."
 npm version $NEW_VERSION
 
 # Add to changelog
@@ -104,8 +118,11 @@ report "writing new git release..."
 git commit -am "release: $NEW_VERSION"
 
 # copy branch onto gh-pages
-report "creating to branch gh-pages..."
+report "creating branch gh-pages..."
 git checkout -b gh-pages
+
+# build it and put it into the release commit
+amend_build
 
 # push to the hub
 report "pushing the latest..."
